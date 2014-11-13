5b4d624f3b01a3a22578246abebf546aecfa6885
rswier <rswier@acm.org>
2014-11-13 17:57:08 -0500

    tidy

--- c4.c
+++ c4.c
@@ -34,7 +34,7 @@ enum {
 // opcodes
 enum { LEA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PSH ,
        OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
-       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT  };
+       OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
 
 // types
 enum { CHAR, INT, PTR };
@@ -48,8 +48,7 @@ next()
 
   while (tk = *p) {
     ++p;
-    if (tk == '\n')
-    {
+    if (tk == '\n') {
       if (src) {
         printf("%d: %.*s", line, p - lp, lp);
         lp = p;
