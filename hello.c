04e84d431bc6e55558b83e405490a53a47c27023
rahulsai9550 <70227643+rahulsai9550@users.noreply.github.com>
2020-10-01 17:39:35 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // "#include" is preprocessor directive , "<stdio.h>" is header file contains standard library I/O functions
 
-int main()
+int main()         // main function which returns int datatype 
 {
+  //when executed, it Prints/Shows hello world
   printf("hello, world\n");
   return 0;
 }
